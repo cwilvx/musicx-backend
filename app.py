f7b39dd555ce2afacd8b146494956132485ee816
geoffrey45 <geoffreymungai45@gmail.com>
2021-08-27 00:44:19 +0300

    get tags from supported files

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
-
+from pathlib import Path
+from mutagen.mp3 import MP3
 
 from flask import Flask
 from flask_cors import CORS
@@ -20,12 +21,36 @@ def get_folders():
         'all_folders': folders
         }
 
+
 @app.route("/<folder>")
 def get_files(folder):
-    files = os.listdir("/home/cwilvx/Music/" + folder)
-    print(files)
+    dir = music_dir + folder
+    print(dir)
+
+    files = []
+
+    for path in Path(dir).rglob('*.flac'):
+        files.append(path.name)
+
+    for path in Path(dir).rglob('*.mp3'):
+        files.append(path.name)
+
     return {'all_files': files}
 
+
+# @app.route("/tags")
+def get_tags():
+    folders = os.listdir(music_dir)
+
+    for folder in folders:
+        dir = music_dir + folder
+
+        for path in Path(dir).rglob('*.flac'):
+            print(path.name)
+            tags = MP3(path)
+            return tags
+    
+get_tags()
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8080, debug=True)
--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 export PORT=9899
 export music_dir="/home/cwilvx/Music"
 
-python server.py &
+# python server.py &
 python app.py
